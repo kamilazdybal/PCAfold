476bc669099e7c98589076dd648d5c8d127d1118
camillejr <kamilazdybal@gmail.com>
2020-03-06 21:13:17 -0700

    Added basic checks; simplified the mixture fraction bins function; added documentation

--- PCA/clustering.py
+++ PCA/clustering.py
@@ -1,17 +1,21 @@
 import numpy as np
-from sklearn.cluster import KMeans
 import random
 import time
 
 def degrade_clusters(idx, verbose=False):
     """
-    This function degrades clusters. It is useful in a case where from a clustering technique
-    you get an idx that does not have a consecutive numeration of clusters.
+    This function degrades cluster numeration if it is composed of non-consecutive integers.
+
     Example:
+    ----------
     If from a clustering technique you get idx that is as following:
-    [0, 0, 2, 0, 5, 10]
+
+    `[0, 0, 2, 0, 5, 10]`
+
     this function turns this idx to:
-    [0, 0, 1, 0, 2, 3]
+
+    `[0, 0, 1, 0, 2, 3]`
+
     where clusters are numbered with consecutive integers.
     """
 
@@ -39,7 +43,7 @@ def degrade_clusters(idx, verbose=False):
 
     return (idx_degraded, k_update)
 
-def variable_bins(var, k, verbose):
+def variable_bins(var, k, verbose=False):
     """
     This function does clustering based on dividing a variable vector `var` into bins
     of equal lengths.
@@ -47,6 +51,10 @@ def variable_bins(var, k, verbose):
        |----------|----------|----------|----------|----------|
           bin 1      bin 2      bin 3       bin 4     bin 5
     """
+
+    # Check that the number of clusters is an integer and is non-zero:
+    if not (isinstance(k, int) and k > 0):
+        raise ValueError("The number of clusters must be a positive integer.")
 
     var_min = np.min(var)
     var_max = np.max(var)
@@ -77,14 +85,22 @@ def variable_bins(var, k, verbose):
 
 def mixture_fraction_bins(Z, k, Z_stoich):
     """
-    This function does clustering based on dividing a mixture fraction vector `Z` into bins
-    of equal lengths. The vector is first split to lean and rich side and then the sides get
-    divided further into clusters.
+    This function does clustering based on dividing a mixture fraction vector
+    `Z` into bins of equal lengths. The vector is first split to lean and rich
+    side and then the sides get divided further into clusters. When k is even,
+    this function will always create equal number of clusters on the lean and
+    rich side. When k is odd, there will be one more cluster on the rich side
+    compared to the lean side.
+
     Z_min           Z_stoich                                 Z_max
        |-------|-------|------------|------------|------------|
          bin 1   bin 2     bin 3        bin 4         bin 5
-    NOTE: This function still hasn't been tested properly, so use with care!
+
     """
+
+    # Check that the number of clusters is an integer and is non-zero:
+    if not (isinstance(k, int) and k > 0):
+        raise ValueError("The number of clusters must be a positive integer.")
 
     # Number of interval borders:
     n_bins_borders = k + 1
@@ -93,7 +109,7 @@ def mixture_fraction_bins(Z, k, Z_stoich):
     min_Z = np.min(Z)
     max_Z = np.max(Z)
 
-    # Partition the Z space:
+    # Partition the Z-space:
     if k == 1:
 
         borders = np.linspace(min_Z, max_Z, n_bins_borders)
@@ -107,7 +123,7 @@ def mixture_fraction_bins(Z, k, Z_stoich):
         borders_rich = np.linspace(Z_stoich, max_Z, np.ceil((n_bins_borders+1)/2))
 
         # Combine the two partitions:
-        borders = np.concatenate((borders_lean[0:int(np.ceil(n_bins_borders/2-1))], borders_rich))
+        borders = np.concatenate((borders_lean[0:-1], borders_rich))
 
     # Bin data matrices initialization:
     idx_clust = []
@@ -125,6 +141,12 @@ def kmeans(X, k):
     """
     This function performs K-Means clustering.
     """
+
+    # Check that the number of clusters is an integer and is non-zero:
+    if not (isinstance(k, int) and k > 0):
+        raise ValueError("The number of clusters must be a positive integer.")
+
+    from sklearn.cluster import KMeans
 
     kmeans = KMeans(n_clusters=k, precompute_distances=True, algorithm='auto').fit(X)
     idx = kmeans.labels_
@@ -146,3 +168,11 @@ def flip_clusters(idx, dictionary):
             flipped_idx.append(i)
 
     return flipped_idx
+
+def test():
+    """
+    This function tests the `clustering` module.
+    """
+
+    print('Test passed.')
+    return 1
