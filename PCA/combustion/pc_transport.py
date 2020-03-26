c76a74c690cd1cfdac6709285d59289884e6ab3a
camillejr <kamilazdybal@gmail.com>
2020-03-26 13:02:01 -0600

    Decreased initial time step

--- PCA/combustion/pc_transport.py
+++ PCA/combustion/pc_transport.py
@@ -15,13 +15,13 @@ def transport_PCs(eta_start, RHS_function):
     governor.maximum_time_step_count = 2e4
     governor.projector_setup_rate = 1
 
-    data = SaveAllDataToList(initial_solution=eta_start, initial_time=0.)
+    data = SaveAllDataToList(initial_solution=eta_start)
 
     governor.custom_post_process_step = data.save_data
 
     governor.integrate(right_hand_side=lambda t, y: RHS_function(y),
                        initial_condition=eta_start,
-                       controller=PIController(first_step=1.e-2, max_step=1.e0, target_error=1.e-8),
+                       controller=PIController(first_step=1.e-8, max_step=1.e0, target_error=1.e-8),
                        method=AdaptiveERK54CashKarp())
 
     return data.solution_list
