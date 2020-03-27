89de10c9e94cc26db8f72873cae86a383c308cea
camillejr <kamilazdybal@gmail.com>
2020-03-26 20:50:34 -0600

    Code clean-up

--- PCA/combustion/data_generation.py
+++ PCA/combustion/data_generation.py
@@ -54,6 +54,21 @@ def homogeneous_reactor(chemical_mechanism, fuel_ratio, mixing_temperature, equi
     fuel = chemical_mechanism.stream('X', fuel_ratio)
     air = chemical_mechanism.stream(stp_air=True)
 
+    # Extract the state-space variables names:
+    number_of_species = chemical_mechanism.n_species
+    species_names = chemical_mechanism.species_names
+    state_space_names = np.array(['T'] + species_names)
+
+    Z_stoich = chemical_mechanism.stoich_mixture_fraction(fuel, air)
+
+    # Initialize matrices that will store the outputs:
+    state_space = np.zeros_like(state_space_names, dtype=float)
+    state_space_sources = np.zeros_like(state_space_names, dtype=float)
+    time_list = np.zeros((1,))
+    number_of_steps = []
+
+    print('Integrating the homogeneous reactor...\n')
+
     for IC_T in mixing_temperature:
 
         for IC_ER in equivalence_ratio:
@@ -64,41 +79,9 @@ def homogeneous_reactor(chemical_mechanism, fuel_ratio, mixing_temperature, equi
             # Set the initial conditions for temperature and pressure:
             mix.TP = IC_T, 101325.
 
-            # Extract the state-space variables names:
-            number_of_species = mix.n_species
-            species_names = mix.species_names
-            state_space_names = np.array(['T'] + species_names)
-
-            Z_stoich = chemical_mechanism.stoich_mixture_fraction(fuel, air)
-
-            if iteration==0:
-
-                print('Integrating the homogeneous reactor...\n')
-
-                # Initialize matrices that will store the outputs:
-                state_space = np.zeros_like(state_space_names, dtype=float)
-                state_space_sources = np.zeros_like(state_space_names, dtype=float)
-                time_list = np.zeros((1,))
-                number_of_steps = []
-
             # Intialize the homogeneous reactor and integrate:
             r = reactors.HomogeneousReactor(chemical_mechanism, mix, 'isobaric', 'adiabatic', 'closed')
             output = r.integrate_to_steady_after_ignition(steady_tolerance=1.e-8, first_time_step=1.e-9, transient_tolerance=1.e-12)
-
-            output = sca.compute_density(chemical_mechanism, output)
-            output = sca.compute_isobaric_specific_heat(chemical_mechanism, output)
-            output = sca.compute_isochoric_specific_heat(chemical_mechanism, output)
-            output = sca.compute_pressure(chemical_mechanism, output)
-            output = sca.compute_specific_enthalpy(chemical_mechanism, output)
-
-            ct_sa, lib_shape = sca.get_ct_solution_array(chemical_mechanism, output)
-            output['mixture_molecular_weight'] = ct_sa.mean_molecular_weight.reshape(lib_shape) # kg/kmol
-            output['energy'] = ct_sa.int_energy_mass.reshape(lib_shape) # specific internal energy J/kg
-            mass_fracs = ct_sa.Y # n_obs x n_spec array of mass fractions
-            production_rates = ct_sa.net_production_rates*ct_sa.molecular_weights # kmol/m^3/s * kg/kmol; n_obs x n_spec array of mass production rates
-            species_enthalpies = ct_sa.standard_enthalpies_RT * ct_sa.T[:,None] * gas_constant / ct_sa.molecular_weights[None,:] # J/kg; n_obs x n_spec array of species enthalpies
-            species_energies = ct_sa.standard_int_energies_RT * ct_sa.T[:,None] * gas_constant / ct_sa.molecular_weights[None,:] # J/kg; n_obs x n_spec array of species internal energies
-            heat_release_rate = -np.sum(production_rates * species_enthalpies,axis=1) / output['density'].ravel() / output['heat capacity cp'].ravel() # K/s; temperature source term
 
             # Get the time:
             time = output.time_values
@@ -108,24 +91,44 @@ def homogeneous_reactor(chemical_mechanism, fuel_ratio, mixing_temperature, equi
             (n_steps, ) = np.shape(time)
             number_of_steps.append(n_steps)
 
+            # Density:
+            output = sca.compute_density(chemical_mechanism, output)
+            density = output['density']
+
+            # Isobaric heat capacity:
+            output = sca.compute_isobaric_specific_heat(chemical_mechanism, output) # J/kg/K
+            isobaric_heat_capacity = output['heat capacity cp']
+
+            ct_sa, lib_shape = sca.get_ct_solution_array(chemical_mechanism, output)
+
+            # Species mass fractions:
+            mass_fractions = ct_sa.Y
+
+            # Temperature:
+            temperature = ct_sa.T[:,None] # K
+
+            # Species production rates:
+            production_rates = ct_sa.net_production_rates * ct_sa.molecular_weights
+            production_rates_over_density = np.divide(production_rates, np.reshape(density, (n_steps, 1)))
+
+            # Species enthalpies:
+            species_enthalpies = ct_sa.standard_enthalpies_RT * temperature * gas_constant / ct_sa.molecular_weights[None,:] # J/kg
+
+            # Species energies:
+            species_energies = ct_sa.standard_int_energies_RT * temperature * gas_constant / ct_sa.molecular_weights[None,:] # J/kg
+
+            # Heat release rate:
+            heat_release_rate = - np.sum(production_rates * species_enthalpies, axis=1) / density.ravel() / isobaric_heat_capacity.ravel() # K/s
+
+            print('it.%.0f \tT: %.0f \tEquivalence ratio: %.2f \tNumber of time steps: %.0f' % (iteration, IC_T, IC_ER, n_steps))
+
             # Intialize the state space matrix at this iteration:
             state_space_at_this_iteration = np.zeros([n_steps, len(state_space_names)])
             state_space_sources_at_this_iteration = np.zeros([n_steps, len(state_space_names)])
 
-            # Get the temperatures:
-            temperature = output['temperature']
-            state_space_at_this_iteration[:,0] = temperature
-            state_space_sources_at_this_iteration[:,0] = heat_release_rate
-
-            # Get the species mass fractions:
-            for i, species in enumerate(species_names):
-
-                species_mass_fraction = output['mass fraction ' + species]
-                state_space_at_this_iteration[:,i+1] = species_mass_fraction
-
-            state_space_sources_at_this_iteration[:,1::] = np.divide(production_rates, np.reshape(output['density'], (n_steps, 1)))
-
-            print('it.%.0f \tT: %.0f \tEquivalence ratio: %.2f \tNumber of time steps: %.0f' % (iteration, IC_T, IC_ER, n_steps))
+            # Populate the solution matrices:
+            state_space_at_this_iteration = np.hstack((temperature, mass_fractions))
+            state_space_sources_at_this_iteration = np.hstack((np.reshape(heat_release_rate, (n_steps,1)), production_rates_over_density))
 
             # Append the current iteration state space to the global state space:
             state_space = np.vstack((state_space, state_space_at_this_iteration))
@@ -202,28 +205,43 @@ def steady_laminar_flamelet(chemical_mechanism, fuel_ratio, initial_condition, d
                                         diss_rate_ref='stoichiometric',
                                         verbose=False)
 
-    output = sca.compute_density(chemical_mechanism, output) # density kg/m^3
-    output = sca.compute_isobaric_specific_heat(chemical_mechanism, output) # heat capacity cp J/kg/K
-    output = sca.compute_isochoric_specific_heat(chemical_mechanism, output) # heat capacity cv J/kg/K
-    output = sca.compute_pressure(chemical_mechanism, output) # pressure Pa
-    output = sca.compute_specific_enthalpy(chemical_mechanism, output) # enthalpy J/kg
+    # Density:
+    output = sca.compute_density(chemical_mechanism, output)
+    density = output['density']
+
+    # Isobaric heat capacity:
+    output = sca.compute_isobaric_specific_heat(chemical_mechanism, output) # J/kg/K
+    isobaric_heat_capacity = output['heat capacity cp']
+
     ct_sa, lib_shape = sca.get_ct_solution_array(chemical_mechanism, output)
-    output['mixture_molecular_weight'] = ct_sa.mean_molecular_weight.reshape(lib_shape) # kg/kmol
-    output['energy'] = ct_sa.int_energy_mass.reshape(lib_shape) # specific internal energy J/kg
-    mass_fracs = ct_sa.Y # n_obs x n_spec array of mass fractions
-    production_rates = ct_sa.net_production_rates*ct_sa.molecular_weights # kmol/m^3/s * kg/kmol; n_obs x n_spec array of mass production rates
-    species_enthalpies = ct_sa.standard_enthalpies_RT * ct_sa.T[:,None] * gas_constant / ct_sa.molecular_weights[None,:] # J/kg; n_obs x n_spec array of species enthalpies
-    species_energies = ct_sa.standard_int_energies_RT * ct_sa.T[:,None] * gas_constant / ct_sa.molecular_weights[None,:] # J/kg; n_obs x n_spec array of species internal energies
-    heat_release_rate = -np.sum(production_rates * species_enthalpies, axis=1) / output['density'].ravel() / output['heat capacity cp'].ravel() # K/s; temperature source term
 
-    n_obs = len(ct_sa.T)
+    # Species mass fractions:
+    mass_fractions = ct_sa.Y
 
-    production_rates = np.divide(production_rates, np.reshape(output['density'], (n_obs, 1)))
+    # Temperature:
+    temperature = ct_sa.T[:,None] # K
 
+    n_obs = len(temperature)
+
+    # Species production rates:
+    production_rates = ct_sa.net_production_rates * ct_sa.molecular_weights
+    production_rates_over_density = np.divide(production_rates, np.reshape(density, (n_obs, 1)))
+
+    # Species enthalpies:
+    species_enthalpies = ct_sa.standard_enthalpies_RT * temperature * gas_constant / ct_sa.molecular_weights[None,:] # J/kg
+
+    # Species energies:
+    species_energies = ct_sa.standard_int_energies_RT * temperature * gas_constant / ct_sa.molecular_weights[None,:] # J/kg
+
+    # Heat release rate:
+    heat_release_rate = - np.sum(production_rates * species_enthalpies, axis=1) / density.ravel() / isobaric_heat_capacity.ravel() # K/s
+
+    # Mixture fraction:
     mixture_fraction = np.reshape(output.mixture_fraction_grid, (n_obs,1))
-    state_space_sources = []
-    state_space_sources = np.hstack((np.reshape(heat_release_rate, (n_obs,1)), production_rates))
-    state_space = np.hstack((ct_sa.T[:,None], ct_sa.Y))
+
+    # Populate the solution matrices:
+    state_space = np.hstack((temperature, mass_fractions))
+    state_space_sources = np.hstack((np.reshape(heat_release_rate, (n_obs,1)), production_rates_over_density))
 
     print('\nDone!')
 
