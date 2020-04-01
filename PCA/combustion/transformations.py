ebe310e7f557c141b8918530877e77f3bbe57845
camillejr <kamilazdybal@gmail.com>
2020-04-01 17:30:04 -0600

    Now allows for scalar inputs of mixture fraction or equivalence ratio

--- PCA/combustion/transformations.py
+++ PCA/combustion/transformations.py
@@ -3,14 +3,14 @@ import numpy as np
 def get_mixture_fraction_from_equivalence_ratio(equivalence_ratio, Z_stoich):
     """
     This function computes mixture fraction vector based on the equivalence
-    ratio vector and the stoichiometric mixture fraction using:
+    ratio and the stoichiometric mixture fraction using:
 
     equivalence_ratio = Z/(1 - Z) * (1 - Z_stoich)/Z_stoich
 
     Input:
     ----------
     `equivalence_ratio`
-               - vector of equivalence ratios.
+               - scalar or vector of equivalence ratio(s).
     `Z_stoich` - stoichiometric mixture fraction.
 
     Output:
@@ -19,25 +19,39 @@ def get_mixture_fraction_from_equivalence_ratio(equivalence_ratio, Z_stoich):
                  element in `equivalence_ratio` vector.
     """
 
-    Z = np.empty([len(equivalence_ratio), 1])
+    if np.isscalar(equivalence_ratio):
 
-    for i in range(0, len(equivalence_ratio)):
+        A = equivalence_ratio * Z_stoich / (1 - Z_stoich)
+        Z = A / (1+A)
 
-        A = equivalence_ratio[i] * Z_stoich / (1 - Z_stoich)
-        Z[i] = A / (1+A)
+    elif len(equivalence_ratio) == 1:
+
+        equivalence_ratio = np.asscalar(np.array(equivalence_ratio))
+
+        A = equivalence_ratio * Z_stoich / (1 - Z_stoich)
+        Z = A / (1+A)
+
+    else:
+
+        Z = np.empty([len(equivalence_ratio), 1])
+
+        for i in range(0, len(equivalence_ratio)):
+
+            A = equivalence_ratio[i] * Z_stoich / (1 - Z_stoich)
+            Z[i] = A / (1+A)
 
     return Z
 
 def get_equivalence_ratio_from_mixture_fraction(Z, Z_stoich):
     """
     This function computes equivalence ratio vector based on the mixture
-    fraction vector and the stoichiometric mixture fraction using:
+    fraction and the stoichiometric mixture fraction using:
 
     equivalence_ratio = Z/(1 - Z) * (1 - Z_stoich)/Z_stoich
 
     Input:
     ----------
-    `Z`        - vector of mixture fractions.
+    `Z`        - scalar or vector of mixture fraction(s).
     `Z_stoich` - stoichiometric mixture fraction.
 
     Output:
@@ -46,10 +60,22 @@ def get_equivalence_ratio_from_mixture_fraction(Z, Z_stoich):
                - vector of equivalence ratios.
     """
 
-    equivalence_ratio = np.empty([len(Z), 1])
+    if np.isscalar(Z):
 
-    for i in range(0, len(Z)):
+        equivalence_ratio = Z/(1 - Z) * (1 - Z_stoich)/Z_stoich
 
-        equivalence_ratio[i] = Z[i]/(1 - Z[i]) * (1 - Z_stoich)/Z_stoich
+    elif len(Z) == 1:
+
+        Z = np.asscalar(np.array(Z))
+
+        equivalence_ratio = Z/(1 - Z) * (1 - Z_stoich)/Z_stoich
+
+    else:
+
+        equivalence_ratio = np.empty([len(Z), 1])
+
+        for i in range(0, len(Z)):
+
+            equivalence_ratio[i] = Z[i]/(1 - Z[i]) * (1 - Z_stoich)/Z_stoich
 
     return equivalence_ratio
