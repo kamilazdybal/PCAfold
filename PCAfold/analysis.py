9df377675df1356b0e02c7af503e93301578ccdc
kamilazdybal <kamilazdybal@gmail.com>
2020-08-31 14:28:13 +0200

    Module headers updates

--- PCAfold/analysis.py
+++ PCAfold/analysis.py
@@ -5,8 +5,8 @@ __copyright__ = "Copyright (c) 2020, Kamila Zdybal and Elizabeth Armstrong"
 __credits__ = ["Department of Chemical Engineering, University of Utah, Salt Lake City, Utah, USA", "Universite Libre de Bruxelles, Aero-Thermo-Mechanics Laboratory, Brussels, Belgium"]
 __license__ = "MIT"
 __version__ = "1.0.0"
-__maintainer__ = "Kamila Zdybal and Elizabeth Armstrong"
-__email__ = "kamilazdybal@gmail.com, James.Sutherland@chemeng.utah.edu"
+__maintainer__ = ["Kamila Zdybal", "Elizabeth Armstrong"]
+__email__ = ["kamilazdybal@gmail.com", "Elizabeth.Armstrong@chemeng.utah.edu", "James.Sutherland@chemeng.utah.edu"]
 __status__ = "Production"
 
 import numpy as np
