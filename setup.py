82e49beb1f891c9918147000544db0459e0f005c
kamilazdybal <kamilazdybal@gmail.com>
2020-09-08 18:39:09 +0200

    Renamed back to PCAfold to sort things out

--- setup.py
+++ setup.py
@@ -11,13 +11,15 @@ is_mac = platform.system() == 'Darwin'
 if is_mac:
     cython_extra_compile_args += ['-stdlib=libc++']
 
-kreg_cython = cythonize(Extension(name='src.kernel_regression',
-                                  sources=[os.path.join('src', 'kernel_regression_cython.pyx')],
+kreg_cython = cythonize(Extension(name='PCAfold.kernel_regression',
+                                  sources=[os.path.join('PCAfold', 'kernel_regression_cython.pyx')],
                                   extra_compile_args=cython_extra_compile_args,
                                   language='c++'))
 
 setup(name='PCAfold',
-      description='Python software for generating and analyzing empirical low-dimensional manifolds obtained via Principal Component Analysis',
+      version='1.0.0',
+      license='MIT',
+      description='Python software for generating, improving and analyzing empirical low-dimensional manifolds obtained via Principal Component Analysis',
       author='Elizabeth Armstrong, Kamila Zdybal',
-      packages=['src'],
+      packages=['PCAfold'],
       ext_modules=kreg_cython)
