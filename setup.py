1a84437818f45bb869d49eecd7a578247007e47f
Elizabeth Armstrong <elizabeth.armstrong@chemeng.utah.edu>
2020-09-07 21:14:33 -0600

    changing code folder to src for SoftwareX

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ if is_mac:
     cython_extra_compile_args += ['-stdlib=libc++']
 
 kreg_cython = cythonize(Extension(name='PCAfold.kernel_regression',
-                                  sources=[os.path.join('PCAfold', 'kernel_regression_cython.pyx')],
+                                  sources=[os.path.join('src', 'kernel_regression_cython.pyx')],
                                   extra_compile_args=cython_extra_compile_args,
                                   language='c++'))
 
 setup(name='PCAfold',
       description='Python software for generating and analyzing empirical low-dimensional manifolds obtained via Principal Component Analysis',
       author='Elizabeth Armstrong, Kamila Zdybal',
-      packages=['PCAfold'],
+      packages=['src'],
       ext_modules=kreg_cython)
