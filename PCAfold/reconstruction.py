efda71a39132a41386905208c95870cf15b230d7
kamilazdybal <kamilazdybal@gmail.com>
2023-04-20 16:10:33 +0200

    Initialized new module reconstruction

---
+++ PCAfold/reconstruction.py
@@ -0,0 +1,10 @@
+"""reconstruction.py: module for reconstruction of QoIs from manifolds."""
+
+__author__ = "Kamila Zdybal, Elizabeth Armstrong, Alessandro Parente and James C. Sutherland"
+__copyright__ = "Copyright (c) 2020-2023, Kamila Zdybal, Elizabeth Armstrong, Alessandro Parente and James C. Sutherland"
+__credits__ = ["Department of Chemical Engineering, University of Utah, Salt Lake City, Utah, USA", "Universite Libre de Bruxelles, Aero-Thermo-Mechanics Laboratory, Brussels, Belgium"]
+__license__ = "MIT"
+__version__ = "2.0.0"
+__maintainer__ = ["Kamila Zdybal", "Elizabeth Armstrong"]
+__email__ = ["kamilazdybal@gmail.com", "Elizabeth.Armstrong@chemeng.utah.edu", "James.Sutherland@chemeng.utah.edu"]
+__status__ = "Production"
